60157ce9469de6354084f00ee2831811cdc0cc5a
LidorAmitay <74604551+LidorAmitay@users.noreply.github.com>
2022-06-14 18:15:37 +0300

    small fixes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,7 @@
 from cv2 import VideoCapture
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+import urllib.request, json
 import random
 import cv2 as cv
 import load_model_and_predict
@@ -11,41 +12,41 @@ app = Flask(__name__)
 CORS(app)
 
 
-bot_convo = {"intents": [
-    {"tag": "greeting",
-     "patterns": ["Hi", "Hey", "Is anyone there?", "Hello", "Hay"],
-     "responses": ["Hello", "Hi", "Hi there"]
-    },
-    {"tag": "goodbye",
-     "patterns": ["Bye", "See you later", "Goodbye"],
-     "responses": ["See you later", "Have a nice day", "Bye! Come back again"]
-    },
-    {"tag": "how ",
-     "patterns": ["Thanks", "Thank you", "That's helpful", "Thanks for the help"],
-     "responses": ["Happy to help!", "Any time!", "My pleasure", "You're most welcome!"]
-    },
-    {"tag": "about",
-     "patterns": ["Who are you?", "What are you?", "Who you are?" ],
-     "responses": ["I.m Joana, your bot assistant", "I'm Joana, an Artificial Intelligent bot"]
-    },
-    {"tag": "name",
-    "patterns": ["what is your name", "what should I call you", "whats your name?"],
-    "responses": ["You can call me Joana.", "I'm Joana!", "Just call me as Joana"]
-    },
-    {"tag": "weather",
-    "patterns": ["How's the weather over there?", "What's the weather right now?", "Is the sun up yet?", "It's raining cats and dogs!"],
-    "responses": ["Tell me how can assist you", "Link to forecast sight", "Yes Sure, How can I support you"]
-    },
-    {"tag": "season",
-    "patterns": ["What is your favorite season", "Summer is the best", "Can't stand the rain", "I have so many allergies right now"],
-    "responses": ["Winter is the season I prefer", "I love the rain and snow", "I don't care, as long as I don't get rusty"]
-    },
-    {"tag": "complaint",
-    "patterns": ["have a complaint", "I want to raise a complaint", "there is a complaint about a service"],
-    "responses": ["Please provide us your complaint in order to assist you", "Please mention your complaint, we will reach you and sorry for any inconvenience caused"]
-    }
-]
-}
+# bot_convo = {"intents": [
+#     {"tag": "greeting",
+#      "patterns": ["Hi", "Hey", "Is anyone there?", "Hello", "Hay"],
+#      "responses": ["Hello", "Hi", "Hi there"]
+#     },
+#     {"tag": "goodbye",
+#      "patterns": ["Bye", "See you later", "Goodbye"],
+#      "responses": ["See you later", "Have a nice day", "Bye! Come back again"]
+#     },
+#     {"tag": "how ",
+#      "patterns": ["Thanks", "Thank you", "That's helpful", "Thanks for the help"],
+#      "responses": ["Happy to help!", "Any time!", "My pleasure", "You're most welcome!"]
+#     },
+#     {"tag": "about",
+#      "patterns": ["Who are you?", "What are you?", "Who you are?" ],
+#      "responses": ["I.m Joana, your bot assistant", "I'm Joana, an Artificial Intelligent bot"]
+#     },
+#     {"tag": "name",
+#     "patterns": ["what is your name", "what should I call you", "whats your name?"],
+#     "responses": ["You can call me Joana.", "I'm Joana!", "Just call me as Joana"]
+#     },
+#     {"tag": "weather",
+#     "patterns": ["How's the weather over there?", "What's the weather right now?", "Is the sun up yet?", "It's raining cats and dogs!"],
+#     "responses": ["Tell me how can assist you", "Link to forecast sight", "Yes Sure, How can I support you"]
+#     },
+#     {"tag": "season",
+#     "patterns": ["What is your favorite season", "Summer is the best", "Can't stand the rain", "I have so many allergies right now"],
+#     "responses": ["Winter is the season I prefer", "I love the rain and snow", "I don't care, as long as I don't get rusty"]
+#     },
+#     {"tag": "complaint",
+#     "patterns": ["have a complaint", "I want to raise a complaint", "there is a complaint about a service"],
+#     "responses": ["Please provide us your complaint in order to assist you", "Please mention your complaint, we will reach you and sorry for any inconvenience caused"]
+#     }
+# ]
+# }
 
 
 def get_args(req, args):
@@ -65,21 +66,21 @@ def check_which_response_to_send(sentence):
     
 
 
-def check_input(input):
-    try:
-        # input_breakdown = nltk.sent_tokenize(input)
-        # response = check_which_response_to_send(input_breakdown)
-        length = len(bot_convo["intents"][0]['responses'])
-        print(length)
-        random_index = random.randint(0, length-1)
-        for greeting in bot_convo["intents"][0]['patterns']:
-            if greeting.lower() == input.lower():
-                return bot_convo["intents"][0]['responses'][random_index]
-        for goodbye in bot_convo["intents"][1]['patterns']:
-            if goodbye.lower() == input.lower():
-                return bot_convo["intents"][1]['responses'][random_index]
-    except Exception as e:
-        print(e) 
+# def check_input(input):
+#     try:
+#         # input_breakdown = nltk.sent_tokenize(input)
+#         # response = check_which_response_to_send(input_breakdown)
+#         length = len(bot_convo["intents"][0]['responses'])
+#         print(length)
+#         random_index = random.randint(0, length-1)
+#         for greeting in bot_convo["intents"][0]['patterns']:
+#             if greeting.lower() == input.lower():
+#                 return bot_convo["intents"][0]['responses'][random_index]
+#         for goodbye in bot_convo["intents"][1]['patterns']:
+#             if goodbye.lower() == input.lower():
+#                 return bot_convo["intents"][1]['responses'][random_index]
+#     except Exception as e:
+#         print(e) 
 
 
 @app.route("/")
@@ -118,11 +119,23 @@ def analyze():
             cv.imwrite("myImage.jpeg", face_only)
             emotion_result = load_model_and_predict.load_and_predict()
 
-            return emotion_result
+            return emotion_result #get_response(emotion_result)
         else:
             return "Error in loading something"
     except Exception as e:
         return e
+
+def get_response(emotion):
+    if emotion == 'sad':
+        url = 'https://v2.jokeapi.dev/joke/Any?type=single'
+        response = urllib.request.urlopen(url)
+        data = response.read()
+        dict = json.loads(data)
+        # resp = "You look sad. maybe this will cheer you up:\n"
+        # resp += dict['joke']
+        return dict['joke'] #resp
+    else:    
+        return emotion
 
 
 if __name__ == '__main__':
